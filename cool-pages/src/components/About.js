18a9b6d24199b9388964dbf569610e971f999987
Ashish <agrawalashish2201@gmail.com>
2022-07-25 13:12:23 +0530

    Final Commit

---
+++ cool-pages/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container my-3">
+            <h1 className="text-center my-5">
+                About Cool Pages
+            </h1>
+            <div> Cool Pages is a <strong>secure way</strong> to take notes. It stores all your notes and categorize them on the basis of the provided tags. Moreover, these notes are <strong>stored on the cloud!</strong> Hence, you can <strong>access the Notes from anywhere</strong> and from any device you wish. Notes can be <strong> edited and deleted as well</strong> with just a single click.  The best part is ... this service is <strong>completely free!!</strong> and you <strong>need not to pay </strong>a single penny to use it. It is <strong>completelety safe and confidential </strong>as well. You can access <strong>only your notes</strong> and nobody else can see your notes. We take care of your <strong> privacy</strong>.</div>
+        </div>
+    )
+}
+
+export default About
